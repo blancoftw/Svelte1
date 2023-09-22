4dfcd6560d76a46458cd6168612be1ffbb4230b7
blancoftw <maxselezen@mail.ru>
2023-09-22 05:09:16 +0000

    готовый App

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+	let numbers = []
+
+	function count (){
+		numbers = [...numbers, numbers.length + 1];
+	}
+	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+
+	function UnCount (idx){
+		// numbers = [...numbers, numbers.delete + 1];
+		numbers = numbers.filter((e,i) => idx != i)
+	}
 </script>
 
-<main>
-	<h1>Привет {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- <p>{numbers.join('  ')} </p> -->
+
+<button on:click={count}>
+	Add a number
+</button>
+
+{#each numbers as num, i}
+	 <!-- svelte-ignore a11y-click-events-have-key-events -->
+	 <p on:click= {() => UnCount(i)}>
+		{num}
+	 </p>
+{/each}
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+p{background-color: aqua;}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+button {
+  border-radius: 6px;
+  padding: 8px 16px;
+  background-color: #5371f4;
+  background-image: url('https://assets.codepen.io/4175254/terrazzo-blue.svg');
+  box-shadow: (
+    inset 0 2px 2px rgb(255 255 255 / .4),
+    inset 0 -2px 1px rgb(0 0 0 / .1),
+    0 1px 2px rgb(0 0 0 / .2),
+    0 2px 4px rgb(0 0 0 / .1),
+  );
+  text-align: center;
+  font-weight: 500;
+  color: #fff;
+  text-shadow: 0 1px rgb(0 0 0 / .2);
+  transition-property: transform;
+  transition-duration: .2s;
+  
+  &:active {
+    transform: scale(.96);
+  }
+}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
